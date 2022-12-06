3e81959340d79a71dc987297337ebfeb5407df33
charlycj <119956843+charlycj@users.noreply.github.com>
2022-12-06 09:59:59 -0600

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,9 @@
+<?php
+date_default_timezone_set('America/Mexico_City');
+error_reporting(0);
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dba = "music";
+$db = new mysqli($host,$user,$pass,$db);
+?>
